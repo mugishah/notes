ece81a37b24edef3f063d75325da9670ce185a3a
HM <mugishah@hotmail.com>
2023-05-02 12:54:15 +0200

    added a auth middleware for accessing a user

--- be/src/routes/users.ts
+++ be/src/routes/users.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as UserController from '../controllers/users'
+import { requiresAuth } from '../middleware/auth';
 
 // Create a new router for the notes
 const router = express.Router()
 
 //register the routes for the CRUD operations
-router.get('/', UserController.getAuthUser)
-router.post('/signup', UserController.signUp)
-router.post('/login', UserController.login)
-router.post('/logout', UserController.logout)
+router.get('/', requiresAuth, UserController.getAuthUser);
+router.post('/signup', UserController.signUp);
+router.post('/login', UserController.login);
+router.post('/logout', UserController.logout);
 
 export default router
