8e03bff0746635a47c571637cdf771c21bb8eaae
HM <mugishah@hotmail.com>
2023-05-02 12:51:33 +0200

    modified all the controllers for notes to included the authentication check

--- be/src/controllers/notes.ts
+++ be/src/controllers/notes.ts
@@ -3,10 +3,17 @@ import Note from "../models/note";
 import { INote, INoteId } from "../interfaces/iNote";
 import createHttpError from "http-errors";
 import mongoose from "mongoose";
+import { assertIsDefined } from "../util/assertIsDefined";
 
 export const getNotes: RequestHandler = async (req, res, next ) => {
+  // take the userId from the session 
+  const authenticatedUserId = req.session.userId;
+
   try {
-    const notes = await Note.find();
+    // check if the userId is defined or not and throw an error if it is not defined
+    assertIsDefined(authenticatedUserId)
+    // find all the notes that have the userId equal to the authenticatedUserId
+    const notes = await Note.find({userId: authenticatedUserId}).exec();
     res.status(200).json(notes);
   } catch (error: any) {
     next(error);
@@ -16,11 +23,22 @@ export const getNotes: RequestHandler = async (req, res, next ) => {
 
 export const getNote: RequestHandler = async (req, res, next ) => {
   // take the id from the url
-  const id = req.params.noteId
+  const noteId = req.params.noteId
+   // take the userId from the session 
+   const authenticatedUserId = req.session.userId;
   try {
-    if(!mongoose.isValidObjectId(id)) throw createHttpError(400, 'Invalid note id');
-    const note = await Note.findById(id).exec();
+    // check if the userId is defined or not and throw an error if it is not defined
+    assertIsDefined(authenticatedUserId)
+
+    if(!mongoose.isValidObjectId(noteId)) throw createHttpError(400, 'Invalid note id');
+
+    const note = await Note.findById(noteId).exec();
+
     if(!note) throw createHttpError(404, 'Note not found');
+
+    //Check if the note belongs to the authenticated user
+    if(!note.userId.equals(authenticatedUserId)) throw createHttpError(403, 'You are not authorized to access this note');
+
     res.status(200).json(note)
   } catch (error) {
     next(error)
@@ -32,11 +50,17 @@ export const createNote: RequestHandler<unknown, unknown, INote, unknown> = asyn
   // take the title and text from the body
   const title = req.body.title;
   const text = req.body.text;
+
+   // take the userId from the session 
+   const authenticatedUserId = req.session.userId;
   try {
+      // check if the userId is defined or not and throw an error if it is not defined
+      assertIsDefined(authenticatedUserId)
 
     if(!title) throw createHttpError(400, 'Note must have a title');
 
     const note = await Note.create({
+      userId: authenticatedUserId,
       title: title, 
       text: text
     });
@@ -47,16 +71,21 @@ export const createNote: RequestHandler<unknown, unknown, INote, unknown> = asyn
 };
 
 export const updateNote: RequestHandler<INoteId, unknown, INote, unknown> = async (req, res, next ) => {
-  const id = req.params.noteId;
+  const noteId = req.params.noteId;
   const title = req.body.title;
   const text = req.body.text;
+   // take the userId from the session 
+   const authenticatedUserId = req.session.userId;
   try {
+    // check if the userId is defined or not and throw an error if it is not defined
+    assertIsDefined(authenticatedUserId)
 
-    if(!mongoose.isValidObjectId(id)) throw createHttpError(400, 'Invalid note id');
+    if(!mongoose.isValidObjectId(noteId)) throw createHttpError(400, 'Invalid note id');
 
     if(!title) throw createHttpError(400, 'Note must have a title');
 
-    const note = await Note.findByIdAndUpdate(id, {
+    const note = await Note.findByIdAndUpdate(noteId, {
+      userId: authenticatedUserId,
       title: title,
       text: text
     }, { new: true }).exec();
@@ -70,10 +99,22 @@ export const updateNote: RequestHandler<INoteId, unknown, INote, unknown> = asyn
 };
 
 export const deleteNote: RequestHandler<INoteId> = async (req, res, next ) => {
-  const id = req.params.noteId;
+  const noteId = req.params.noteId;
+  // take the userId from the session 
+  const authenticatedUserId = req.session.userId;
   try {
-    if(!mongoose.isValidObjectId(id)) throw createHttpError(400, 'Invalid note id');
-    const note = await Note.findByIdAndDelete(id).exec();
+
+    // check if the userId is defined or not and throw an error if it is not defined
+    assertIsDefined(authenticatedUserId)
+
+    if(!mongoose.isValidObjectId(noteId)) throw createHttpError(400, 'Invalid note id');
+
+    const note = await Note.findByIdAndDelete({
+      authenticatedUserId,
+      noteId
+    }).exec();
+  
+
     if(!note) throw createHttpError(404, 'Note not found');
     res.sendStatus(204);
   } catch (error) {
